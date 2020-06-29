c04f461e81003575c2320aa8619ec416961ad341
kazuhi-ra <kazuhira4126@gmail.com>
2020-06-29 20:40:44 +0900

    ドラッグ&ドロップ機能の実装をしました

--- src/Sell.js
+++ src/Sell.js
@@ -1,53 +1,14 @@
-import React, { useCallback, useState } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
-import Dropzone from 'react-dropzone'
+import Dnd from './Dnd'
 
-// 参考資料
-// https://dev.to/bnevilleoneill/the-ultimate-guide-to-drag-and-drop-in-react-5955
-// https://shiro-goma.hatenablog.com/entry/2016/02/29/034215
 const Sell = () => {
-  const [itemImages, setItemImages] = useState([])
-
-  const onDrop = useCallback(acceptedFiles => {
-    acceptedFiles.map(file => {
-      console.log(file)
-      // ファイルをバイナリ文字列に変換するため、FileReader オブジェクトを使用
-      const reader = new FileReader()
-      reader.readAsDataURL(file)
-      reader.onload = e => {
-        setItemImages(prevState => [...prevState, e.target.result])
-      }
-      return file
-    })
-  }, [])
-
   return (
     <>
       <Helmet>
         <title>Sell.js</title>
       </Helmet>
-      {/* acceptedFilesに画像が格納される */}
-      <Dropzone
-        accept={'image/*'}
-        onDrop={acceptedFiles => onDrop(acceptedFiles)}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              <p>
-                ドラッグアンドドロップかここをクリックで画像をアップできます。
-                <br />
-                今は画像をアップするとconsoleに内容が表示されます。
-              </p>
-              <p>形式: png/jpeg/jpg</p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      <img src={itemImages[0]} alt='アップロードした写真' width="200" />
-      <img src={itemImages[1]} alt='アップロードした写真' />
-      <img src={itemImages[2]} alt='アップロードした写真' />
+      <Dnd />
     </>
   )
 }
