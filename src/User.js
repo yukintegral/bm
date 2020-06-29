1462774cf6e3ef7073a74935eba15ba9fbce0e19
kazuhi-ra <kazuhira4126@gmail.com>
2020-06-29 10:54:31 +0900

    ユーザーページを生やしました

---
+++ src/User.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const User = () => {
+  return (
+    <>
+      <Helmet>
+        <title>User</title>
+      </Helmet>
+      <div>ユーザーの紹介ページです</div>
+    </>
+  )
+}
+
+export default User
