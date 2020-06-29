184ddc04f2256ddf6734b5f662eec871d31c8dee
kazuhi-ra <kazuhira4126@gmail.com>
2020-06-29 10:35:52 +0900

    Itemページにhelmetを追加

--- src/Item.js
+++ src/Item.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const Item = () => {
   return (
     <>
+      <Helmet>
+        <title>Item</title>
+      </Helmet>
       商品をクリックした先の詳細ページです
     </>
   )
