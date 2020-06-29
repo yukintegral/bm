40de0b3d6c813bdb4d3b4b443a6be9c6f72c22dd
kazuhi-ra <kazuhira4126@gmail.com>
2020-06-29 10:33:51 +0900

    Top.jsにhelmetを追加

--- src/Top.js
+++ src/Top.js
@@ -1,9 +1,13 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
 
 const Top = () => {
   return (
     <>
+      <Helmet>
+        <title></title>
+      </Helmet>
       <div>Topページです</div>
     </>
   )
